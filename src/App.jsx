91b9a98e81b6b1904852ef49b962802fc3836c6a
Siddesh Patil <siddeshp388@gmail.com>
2024-09-21 14:21:36 +0530

    removed sentry

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Highlights from "./components/Highlights";
 import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
-import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
